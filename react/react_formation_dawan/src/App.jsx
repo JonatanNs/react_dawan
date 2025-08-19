4d3bf9137069006f6aca298aa377ba451dc5446d
Jonatan Ns <nsualu.jonatan@gmail.com>
2025-08-19 11:22:33 +0200

    feat : add hookUseState

--- react/react_formation_dawan/src/App.jsx
+++ react/react_formation_dawan/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import JsxTheorie from "./components/JsxTheorie";
+import HookUseState from './components/HookUseState';
 
 function App(){
   return (
@@ -8,11 +9,11 @@ function App(){
       <p>NavBar</p>
       <BrowserRouter>
         <Routes>
-          <Route path="" element={ <JsxTheorie /> } />
+          <Route path="/jsx" element={ <JsxTheorie /> } />
+          <Route path="/useState" element={ <HookUseState /> } />
           <Route path="/test" element={ <> <p>Page test !</p> </> } />
         </Routes>
       </BrowserRouter>
-      <JsxTheorie/>
     </>
   )
 }
---
+++ react/react_formation_dawan/src/components/HookUseState.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { hydrateRoot } from "react-dom/client"
+
+function HookUseState(){
+
+    const [cpt, setCpt] = useState(0);
+
+    console.log("cpt", cpt);
+
+    const increment = () =>{
+        setCpt((prevValue) => prevValue +1);
+    }
+
+    const decrement = () =>{
+        setCpt((prevValue) => prevValue -1);
+    }
+
+    const reset = () =>{
+        setCpt(0);
+    }
+
+    return (
+        <>
+            <h1>Le hook use state</h1>
+            <p>{cpt}</p>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+            <button onClick={reset}>Reset</button>
+
+        </>
+    )
+}
+
+export default HookUseState
